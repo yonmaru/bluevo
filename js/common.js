97c588df6ed6d87c3590ea70757269dd5351eae4
モコクリ松口 <youichi@mococreations.com>
2019-04-25 09:00:34 +0900

    とりあえずコーディング完了

---
+++ js/common.js
@@ -0,0 +1,72 @@
+// JavaScript Document
+
+//画像切り替え
+$(function() {
+  // 置換の対象とするclass属性。
+  var $elem = $('.image-switch');
+  // 置換の対象とするsrc属性の末尾の文字列。
+  var sp = '_sp.';
+  var pc = '_pc.';
+  // 画像を切り替えるウィンドウサイズ。
+  var replaceWidth = 641;
+
+  function imageSwitch() {
+    // ウィンドウサイズを取得する。
+    var windowWidth = parseInt($(window).width());
+
+    // ページ内にあるすべての`.image-switch`に適応される。
+    $elem.each(function() {
+      var $this = $(this);
+      // ウィンドウサイズが641px以上であれば_spを_pcに置換する。
+      if(windowWidth >= replaceWidth) {
+        $this.attr('src', $this.attr('src').replace(sp, pc));
+
+      // ウィンドウサイズが640px未満であれば_pcを_spに置換する。
+      } else {
+        $this.attr('src', $this.attr('src').replace(pc, sp));
+      }
+    });
+  }
+  imageSwitch();
+
+  // 動的なリサイズは操作後0.2秒経ってから処理を実行する。
+  var resizeTimer;
+  $(window).on('resize', function() {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function() {
+      imageSwitch();
+    }, 100);
+  });
+
+    setTimeout(function () {
+        document.getElementsByTagName("html")[0].classList.add("loading-delay");
+    }, 2000);
+    
+    
+    $('#menu').click(function(){
+        $(this).toggleClass('open');
+        $('#header ul').slideToggle(100);
+    });
+        
+    var windowWidth = $(window).width();
+    var windowSm = 640;
+    if (windowWidth <= windowSm) {
+        $('#header li a').click(function(){
+            $('#header ul').slideUp(100);
+            $('#menu').removeClass('open');
+        });
+    }
+    
+    var headerHight = 40; //ヘッダの高さ
+    var speed = 400;
+    $('a[href^=#]').click(function(){
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top-headerHight; //ヘッダの高さ分位置をずらす
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+
+});
+
+
